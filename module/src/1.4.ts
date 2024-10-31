c548fff7b83541338872ec213f7af42fb7ace02a
myusername <farhadhossen2590@gmail.com>
2024-10-31 21:01:31 +0600

    New Updates in Igbore button

---
+++ module/src/1.4.ts
@@ -0,0 +1,16 @@
+//data type
+
+//string
+// implicid data type 
+let firstName = 'Farhad'
+//explecid data type
+let firstSName : string = 'Farhad'
+
+//number 
+let roll = 35
+//boolean
+let isAdmin : boolean = true
+//undefined 
+let x : undefined = undefined
+//null
+let z : null = null
