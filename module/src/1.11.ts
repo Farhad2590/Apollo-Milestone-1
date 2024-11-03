e31a508cb78cc1f2ea552b08f115f942f53dda8a
myusername <farhadhossen2590@gmail.com>
2024-11-04 01:00:14 +0600

    Practice Done

--- module/src/1.11.ts
+++ module/src/1.11.ts
@@ -12,6 +12,33 @@
 
     const isAdult = age >= 18 ? "adult" : "not adult";
 
+    console.log({ isAdult });
+
+    const isAuthenticated = null
+    const result = isAuthenticated ?? 'Guest'
+    console.log(result);
+    type User = {
+        name: string;
+        address: {
+            city: string;
+            road: string;
+            presentaddress: string;
+            permanentAddress?: string;
+        };
+    };
+
+    const user: User = {
+        name: "Persian",
+        address: {
+            city: "ctg",
+            road: "Awesome Road",
+            presentaddress: "ctg town",
+        },
+    };
+
+    const permanentAddress =
+        user?.address?.permanentAddress ?? "No Permanent Address";
+    console.log({ permanentAddress });
 
 
 
